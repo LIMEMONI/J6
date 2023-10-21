37377407ada03ca2c92289ad6d54badbeba0586d
LIMEMONI <125669311+LIMEMONI@users.noreply.github.com>
2023-10-21 15:42:11 +0900

    ubuntu_연결완료_다중모델 연결 확인

--- model_conn_3.py
+++ model_conn_3.py
@@ -38,7 +38,7 @@ def fetch_recent_logs(length=avg_len):
             FLOWCOOLFLOWRATE, FLOWCOOLPRESSURE, ETCHGASCHANNEL1READBACK, ETCHPBNGASREADBACK,
             FIXTURETILTANGLE, ROTATIONSPEED, ACTUALROTATIONANGLE,
             ETCHSOURCEUSAGE, ETCHAUXSOURCETIMER, ETCHAUX2SOURCETIMER,
-            ACTUALSTEPDURATION FROM input_data ORDER BY input_time DESC LIMIT {length}'''
+            ACTUALSTEPDURATION FROM input_data_3 ORDER BY input_time DESC LIMIT {length}'''
             cursor.execute(sql)
             results = cursor.fetchall()
     finally:
@@ -64,10 +64,9 @@ def fetch_recent_logs_for_multi(length=avg_len):
     try:
         with connection.cursor() as cursor:
             # 가장 최근의 데이터부터 지정한 길이만큼 가져오는 SQL 쿼리
-            sql = f'''SELECT IONGAUGEPRESSURE, ETCHBEAMVOLTAGE, ETCHSUPPRESSORCURRENT,
-       FLOWCOOLFLOWRATE, FLOWCOOLPRESSURE, ETCHGASCHANNEL1READBACK,
-       ETCHPBNGASREADBACK, FIXTURETILTANGLE, ACTUALROTATIONANGLE,
-       ETCHSOURCEUSAGE FROM input_data ORDER BY input_time DESC LIMIT {length}'''
+            sql = f'''SELECT ACTUALROTATIONANGLE, ACTUALSTEPDURATION, ETCHBEAMCURRENT, ETCHGASCHANNEL1READBACK, 
+              ETCHPBNGASREADBACK, ETCHSOURCEUSAGE, FIXTURETILTANGLE, FLOWCOOLFLOWRATE, FLOWCOOLPRESSURE, 
+              IONGAUGEPRESSURE FROM input_data_3 ORDER BY input_time DESC LIMIT {length}'''
             cursor.execute(sql)
             results = cursor.fetchall()
     finally:
@@ -87,7 +86,7 @@ def predict_with_xgb_model(data):
 
     """xgboost 모델을 사용해 예측하는 함수"""
     # 모델 불러오기
-    with open('./xgboost_model.pkl', 'rb') as f:
+    with open('./model_data_input/xgboost_model.pkl', 'rb') as f:
         model = pickle.load(f)
 
     # 예측 실행
@@ -102,7 +101,7 @@ def predict_with_xgb_multi_model(data):
 
     """xgboost 모델을 사용해 예측하는 함수"""
     # 모델 불러오기
-    with open('./xgboost_multi_model.pickle', 'rb') as f:
+    with open('./model_data_input/xgboost_multi_model.pkl', 'rb') as f:
         model = pickle.load(f)
 
     # 예측 실행
@@ -127,34 +126,11 @@ def compute_moving_median(data, window_size):
 
     return np.array(medians)
 
+########################################################################################################################################
+########################################################################################################################################
+########################################################################################################################################
 
-def insert_single_rul_data(connection, single_data):
-    try:
-        with connection.cursor() as cursor:
-            # 현재 시간 가져오기
-            current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            
-            # 데이터 삽입 SQL.
-            sql = f'''INSERT INTO rul(rul_time,input_time) VALUES (%s,"{current_time}")'''
-            cursor.execute(sql, single_data)
-        connection.commit()
-    except Exception as e:
-        print(f"Error while inserting data: {e}")
-        connection.rollback()
-
-def insert_single_multi_data(connection, single_data):
-    try:
-        with connection.cursor() as cursor:
-            # 현재 시간 가져오기
-            current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            
-            # 데이터 삽입 SQL.
-            sql = f'''INSERT INTO multi(multi_pred,input_time) VALUES (%s,"{current_time}")'''
-            cursor.execute(sql, single_data)
-        connection.commit()
-    except Exception as e:
-        print(f"Error while inserting data: {e}")
-        connection.rollback()
+### 데이터 밀어 넣기
 
 def insert_single_data(connection, single_data):
     try:
@@ -162,7 +138,7 @@ def insert_single_data(connection, single_data):
             # 현재 시간 가져오기
             current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             # 데이터 삽입 SQL.
-            sql = f'''INSERT INTO input_data (time, Tool, stage, Lot, runnum, recipe, recipe_step,
+            sql = f'''INSERT INTO input_data_3 (time, Tool, stage, Lot, runnum, recipe, recipe_step,
        IONGAUGEPRESSURE, ETCHBEAMVOLTAGE, ETCHBEAMCURRENT,
        ETCHSUPPRESSORVOLTAGE, ETCHSUPPRESSORCURRENT, FLOWCOOLFLOWRATE,
        FLOWCOOLPRESSURE, ETCHGASCHANNEL1READBACK, ETCHPBNGASREADBACK,
@@ -174,7 +150,39 @@ def insert_single_data(connection, single_data):
     except Exception as e:
         print(f"Error while inserting data: {e}")
         connection.rollback()
+    return current_time
 
+def insert_single_rul_data(connection, single_data, current_time):
+    try:
+        with connection.cursor() as cursor:
+            # 현재 시간 가져오기
+            current_time = current_time
+            
+            # 데이터 삽입 SQL.
+            sql = f'''INSERT INTO rul_3(rul_time,input_time) VALUES (%s,"{current_time}")'''
+            cursor.execute(sql, single_data)
+        connection.commit()
+    except Exception as e:
+        print(f"Error while inserting data: {e}")
+        connection.rollback()
+
+def insert_single_multi_data(connection, single_data, current_time):
+    try:
+        with connection.cursor() as cursor:
+            # 현재 시간 가져오기
+            current_time = current_time
+            
+            # 데이터 삽입 SQL.
+            sql = f'''INSERT INTO multi_3(multi_pred,input_time) VALUES (%s,"{current_time}")'''
+            cursor.execute(sql, single_data)
+        connection.commit()
+    except Exception as e:
+        print(f"Error while inserting data: {e}")
+        connection.rollback()
+
+########################################################################################################################################
+########################################################################################################################################
+########################################################################################################################################
 
 
 def main():
@@ -193,16 +201,18 @@ def main():
                                  cursorclass=pymysql.cursors.DictCursor)
     
     # CSV 파일 읽기
-    df = pd.read_csv("./test.csv")
+    df = pd.read_csv("./model_data_input/test.csv")
 
     # DataFrame에서 튜플 리스트로 데이터 변환
     data_tuples = list(df.itertuples(index=False, name=None))
+
+    ## 데이터를 한줄 씩 밀어넣으면서 진행하는 방식
 
     for single_data in data_tuples:
 
         try:
             # 데이터 삽입
-            insert_single_data(connection, single_data)
+            current_time = insert_single_data(connection, single_data)
 
             data = fetch_recent_logs(length=avg_len)
             data_for_multi = fetch_recent_logs_for_multi(length=avg_len)
@@ -220,8 +230,8 @@ def main():
             # print(moving_avg)  # 계산된 이동평균을 출력합니다. 필요에 따라 다른 처리를 할 수 있습니다.
             
             # rul 데이터 삽입
-            insert_single_rul_data(connection, moving_avg[0])
-            insert_single_multi_data(connection, moving_avg_for_multi[0])
+            insert_single_rul_data(connection, moving_avg[0], current_time)
+            insert_single_multi_data(connection, predictions_for_multi[0], current_time)
             # print(f"Inserted a row from CSV.")
             time.sleep(4)  # 4초 대기
 
